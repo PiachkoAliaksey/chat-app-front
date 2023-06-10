48a705d42de41b94208b139961dba5b2004c1f76
PiachkoAliaksey <alek17y@mail.ru>
2023-06-10 20:35:54 +0300

    add: MUI component Skeleton for users

---
+++ src/components/SkeletonUsers/SkeletonUsers.tsx
@@ -0,0 +1,25 @@
+
+import React from "react";
+import { Skeleton } from "@mui/material";
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import Avatar from '@mui/material/Avatar';
+
+
+
+export const SkeletonUsers = () => {
+
+  return (
+    <>
+      {([...new Array(5)].map((user, index) => (<ListItemButton >
+        <ListItemIcon>
+          <Skeleton variant="circular" width={40} height={40} />
+        </ListItemIcon>
+        <Skeleton variant="text" sx={{ fontSize: '20rem' }} />
+      </ListItemButton>)))}
+
+    </>
+  )
+}
